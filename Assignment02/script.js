670a3a8c746a264620adb7bf7c5a95ee2eeea03b
mattmelio <70667820+mattmelio@users.noreply.github.com>
2020-10-08 19:22:30 -0400

    Update script.js

--- Assignment02/script.js
+++ Assignment02/script.js
@@ -1,2 +1,14 @@
+//creates array 
 var tasks = [];
+
+// sets priority 
 var priority = document.getElementById("priority");
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
