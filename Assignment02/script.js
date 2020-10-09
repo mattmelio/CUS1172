ba2b9619660b23db127bef2a09f11a7c6fc870b0
mattmelio <70667820+mattmelio@users.noreply.github.com>
2020-10-08 21:28:25 -0400

    Update script.js

--- Assignment02/script.js
+++ Assignment02/script.js
@@ -52,7 +52,32 @@ for (i = 0; i < close.length; i++) {
       })
    });
   
-  
+ // Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+ //Add new task to tasks array
+  tasks.push(inputValue);
+  console.log(tasks[tasks.length - 1] + " added to tasks");
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
                                                      
                             
                      
